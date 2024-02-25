8e8712d94f569aae3fd8ebdf8a3cb0b786b87be1
drdrumm911 <oren.carm@gmail.com>
2024-02-25 11:26:06 +0200

    Another Commit

--- MySoftware.py
+++ MySoftware.py
@@ -1,1 +1,9 @@
-Test
+class Screen:
+    def welcome(self):
+        print("New Screen")
+
+# Create an instance of the class
+screen_instance = Screen()
+
+# Call the welcome() method
+screen_instance.welcome()  # This will print "New Screen"
