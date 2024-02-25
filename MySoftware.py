ef26e4a9d833115e3785836b24648c0d1ae84f86
drdrumm911 <oren.carm@gmail.com>
2024-02-25 12:05:21 +0200

    Another Commit

--- MySoftware.py
+++ MySoftware.py
@@ -1,3 +1,11 @@
+class Button:
+    def click(self):
+        print("Click")
+
+# You can create an instance of the class and call the click() method
+button_instance = Button()
+button_instance.click()  # This will print "Click"
+
 class Screen:
     def welcome(self):
         print("New Screen")
@@ -8,11 +16,5 @@ screen_instance = Screen()
 # Call the welcome() method
 screen_instance.welcome()  # This will print "New Screen"
 
-class Button:
-    def click(self):
-        print("Click")
 
-# You can create an instance of the class and call the click() method
-button_instance = Button()
-button_instance.click()  # This will print "Click"
 
