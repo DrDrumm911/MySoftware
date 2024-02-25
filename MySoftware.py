ab8c5bd19c53b8024fd5eb82c29f5840c2c5e144
drdrumm911 <oren.carm@gmail.com>
2024-02-25 11:26:46 +0200

    Another Commit

--- MySoftware.py
+++ MySoftware.py
@@ -1,1 +1,7 @@
-Test
+class Button:
+    def click(self):
+        print("Click")
+
+# You can create an instance of the class and call the click() method
+button_instance = Button()
+button_instance.click()  # This will print "Click"
